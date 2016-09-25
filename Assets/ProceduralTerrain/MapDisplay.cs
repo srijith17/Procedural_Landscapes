58c1e12ae9425f22db3738d9854abbddb6f09d04
Srijith Nair <srijith17@gmail.com>
2016-09-25 16:59:37 -0700

    Random Map with Heights From Tutorial

--- Assets/ProceduralTerrain/MapDisplay.cs
+++ Assets/ProceduralTerrain/MapDisplay.cs
@@ -2,26 +2,21 @@
 using UnityEngine;
 using System.Collections;
 
-public class MapDisplay : MonoBehaviour {
-	public Renderer textureRenderer;
+public class MapDisplay : MonoBehaviour
+{
+    public Renderer textureRenderer;
+    public MeshFilter meshFilter;
+    public MeshRenderer meshRenderer;
 
-	public void DrawNoiseMap(float[,] noiseMap){
-		int width = noiseMap.GetLength (0);
-		int height = noiseMap.GetLength (1);
+    public void DrawTexture(Texture2D texture)
+    {
+        textureRenderer.sharedMaterial.mainTexture = texture;
+        textureRenderer.transform.localScale = new Vector3(texture.width, 1, texture.height);
+    }
 
-		Texture2D texture = new Texture2D (width, height);
-
-		Color[] colorMap = new Color [width * height];
-		for (int y = 0; y < height; y++) {
-			for (int x = 0; x < width; x++) {
-				//(x * wdith + y) ?
-				colorMap[y*width +x] = Color.Lerp(Color.black, Color.white, noiseMap[x,y]);
-			}
-		}
-		texture.SetPixels (colorMap);
-		texture.Apply ();
-
-		textureRenderer.sharedMaterial.mainTexture = texture;
-		textureRenderer.transform.localScale = new Vector3 (width, 1, height);
-	}
+    public void DrawMesh(MeshData meshData, Texture2D texture)
+    {
+        meshFilter.sharedMesh = meshData.CreateMesh();
+        meshRenderer.sharedMaterial.mainTexture = texture;
+    }
 }
