2b04407c3057a1078cd9e30491d010a4e428bcdf
Srijith Nair <srijith17@gmail.com>
2016-09-27 23:25:34 -0700

    Scrweing around. added stubs

---
+++ Assets/ProceduralTerrain/Editors/HeightMapGeneratorEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor (typeof (HeightMapGenerator))]
+public class HeightMapGeneratorEditor : Editor {
+	public override void OnInspectorGUI(){
+		HeightMapGenerator heightMapGen = (HeightMapGenerator)target;
+
+		if (DrawDefaultInspector ()) {
+			if (heightMapGen.autoUpdate) {
+				heightMapGen.GenerateMap ();
+			}
+		}
+
+		if (GUILayout.Button ("Generate")) {
+			heightMapGen.GenerateMap ();
+		}
+	}
+}
