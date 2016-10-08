580f0e3951d7e3c7b87bcaf57fb4c3e2ec6a17e6
Roshan Muralidharan <rosh89@gmail.com>
2016-10-08 12:11:51 -0500

    Endless terrain

--- Assets/ProceduralTerrain/Scripts/EndlessTerrain.cs
+++ Assets/ProceduralTerrain/Scripts/EndlessTerrain.cs
@@ -12,13 +12,25 @@ public class EndlessTerrain : MonoBehaviour {
 	int chunksVisibleInViewDst;
 
 	Dictionary<Vector2, TerrainChunk> terrainChunkDictionary = new Dictionary<Vector2, TerrainChunk>();
+	List<TerrainChunk> terrainChunksVisibleLastUpdate = new List<TerrainChunk>();
 
 	void Start() {
-		chunkSize = NoiseMapGenerator.mapChunkSize - 1;
+		chunkSize = HeightMapGenerator.mapChunkSize - 1;
 		chunksVisibleInViewDst = Mathf.RoundToInt(maxViewDst / chunkSize);
 	}
 
+	void Update(){
+		viewerPosition = new Vector2 (viewer.position.x, viewer.position.z);
+		UpdateVisibleChunks ();
+	}
+
 	void UpdateVisibleChunks(){
+
+		foreach (TerrainChunk chunk in terrainChunksVisibleLastUpdate){
+			chunk.SetVisible(false);
+		}
+		terrainChunksVisibleLastUpdate.Clear ();  
+
 		int currentChunkCoordX = Mathf.RoundToInt (viewerPosition.x / chunkSize);
 		int currentChunkCoordY = Mathf.RoundToInt (viewerPosition.y / chunkSize);
 
@@ -27,13 +39,48 @@ public class EndlessTerrain : MonoBehaviour {
 				Vector2 viewedChunkCoord = new Vector2 (currentChunkCoordX + xOffSet, currentChunkCoordY + yOffSet);
 
 				if (!terrainChunkDictionary.ContainsKey (viewedChunkCoord)) {
-					terrainChunkDictionary.Add (viewedChunkCoord, new TerrainChunk ());
+					terrainChunkDictionary.Add (viewedChunkCoord, new TerrainChunk (viewedChunkCoord, chunkSize, transform));
+				} else {
+					terrainChunkDictionary [viewedChunkCoord].UpdateTerrainChunk ();
+					if (terrainChunkDictionary [viewedChunkCoord].isVisible ()) {
+						terrainChunksVisibleLastUpdate.Add (terrainChunkDictionary [viewedChunkCoord]);
+					}
 				}
 			}
 		}
 	}
 
 	public class TerrainChunk{
-		
+
+		GameObject meshObject;
+		Vector2 position;
+		Bounds bounds;
+
+		public TerrainChunk(Vector2 coord, int size, Transform parent){
+			position = coord *size;
+			bounds = new Bounds(position, Vector2.one * size);
+			Vector3 positionV3 = new Vector3(position.x, 0, position.y);
+
+			meshObject = GameObject.CreatePrimitive(PrimitiveType.Plane);
+			meshObject.transform.position = positionV3;
+			meshObject.transform.localScale = Vector3.one * size /10f;
+			meshObject.transform.parent = parent;
+
+			SetVisible(false);
+		}
+
+		public void UpdateTerrainChunk(){
+			float viewerDstFromEdge = bounds.SqrDistance (viewerPosition);
+			bool visible = viewerDstFromEdge <= maxViewDst;
+			SetVisible (visible);
+		}
+
+		public void SetVisible(bool vis){
+			meshObject.SetActive (vis);
+		}
+
+		public bool isVisible(){
+			return meshObject.activeSelf;
+		}
 	}
 }
